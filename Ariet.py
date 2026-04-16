9e71ebd180f6c3f5535f192031a0d1119c3de065
agenta67 <ariet8699@gmail.com>
2026-04-16 16:43:19 +0600

    import library

--- Ariet.py
+++ Ariet.py
@@ -1,7 +1,2 @@
 import random
 import string
-print("=== Генератор никнеймов ===")
-fantasy = ["Dragon", "Shadow", "Knight", "Wizard", "Blade", "Elf"]
-cyber = ["Neo", "Cyber", "Byte", "Zero", "Glitch", "Core"]
-common = ["Fast", "Cool", "Lucky", "Crazy", "Pro", "Mega"]
-
