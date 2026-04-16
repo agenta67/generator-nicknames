cbd381280a5ecd177f6d30dfc07d8429f8d3814b
agenta67 <ariet8699@gmail.com>
2026-04-17 02:15:42 +1100

    Add input for nickname length in Ariet.py

--- Ariet.py
+++ Ariet.py
@@ -5,3 +5,4 @@ fantasy = ["Dragon", "Shadow", "Knight", "Wizard", "Blade", "Elf"]
 cyber = ["Neo", "Cyber", "Byte", "Zero", "Glitch", "Core"]
 common = ["Fast", "Cool", "Lucky", "Crazy", "Pro", "Mega"]
 mode = int(input("Тип ника (1-осмысленный, 2-рандом): ").strip())
+length = int(input("Длина ника: ").strip())
