27cf28e7c26d166a1ad4a8ebfa44297536d648b4
agenta67 <ariet8699@gmail.com>
2026-04-21 02:32:04 +1100

    Add infinite loop for continuous nickname generation

--- Ariet.py
+++ Ariet.py
@@ -3,3 +3,4 @@ print("=== Nickname Generator ===")
 fantasy = ["Dragon", "Shadow", "Knight", "Wizard", "Blade", "Elf","Necromancy","Sorcery","Goblin","Dwarf","Overlord"]
 cyber = ["Neo", "Cyber", "Byte", "Zero", "Glitch", "Core","Byte","Hacker","Vector","Binary","Exe","Node","Secure"]
 common = ["Fast", "Cool", "Lucky", "Crazy", "Pro", "Mega","Sigma","Alpha","Beta","Sahur","Overvoltage","Nimble"]
+while True:
